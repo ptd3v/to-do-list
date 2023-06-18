312dac014d6c2ef84a82892a2ec044bcbc078b75
ptd3v <102877038+ptd3v@users.noreply.github.com>
2023-06-18 13:13:10 +0100

    Extensive comments added

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # to-do-list-javascript
- A simple to do list I created to get some fresh experience with JavaScript.
+A simple to do list I created to get some fresh experience with JavaScript.
+
+You can add a task by writing in the text box and pressing the 'Add Task' button.
+Once added, it will be added to a temporary list with a checkbox next to the item.
+
+Click the checkbox when the task is completed and it will be given a strike through.
+Click the button again to remove the strike through from the task.
--- script.js
+++ script.js
@@ -1,32 +1,54 @@
 // script.js
+
+// This function adds a task to a to-do list
 function addTask() {
+  // Retrieve the task input element from the HTML
   var taskInput = document.getElementById("taskInput");
+
+  // Get the value entered in the task input
   var task = taskInput.value;
 
+  // Check if the task is not empty
   if (task !== "") {
+    // Create a new list item element
     var listItem = document.createElement("li");
+
+    // Create a checkbox element
     var checkbox = document.createElement("input");
     checkbox.type = "checkbox";
+
+    // Add an event listener to the checkbox to handle task completion
     checkbox.addEventListener("change", toggleTaskCompletion);
 
+    // Create a label element to display the task text
     var label = document.createElement("label");
     label.innerText = task;
 
+    // Append the checkbox and label to the list item
     listItem.appendChild(checkbox);
     listItem.appendChild(label);
+
+    // Append the list item to the to-do list
     document.getElementById("todo-list").appendChild(listItem);
 
+    // Reset the task input value to an empty string
     taskInput.value = "";
   }
 }
 
+// This function toggles the completion status of a task
 function toggleTaskCompletion(event) {
+  // Get the checkbox that triggered the event
   var checkbox = event.target;
+
+  // Get the parent list item of the checkbox
   var listItem = checkbox.parentNode;
 
+  // If the checkbox is checked, add the "completed" class to the list item
   if (checkbox.checked) {
     listItem.classList.add("completed");
   } else {
+    // If the checkbox is unchecked, remove the "completed" class from the list item
     listItem.classList.remove("completed");
   }
 }
